52c3d39195f5281a18a7b76b7085d16f288ae407
PaulR <paulr756756@gmail.com>
2021-01-31 19:32:18 +0530

    Added leetcode

---
+++ leetcode/1.two-sum.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i=0, j=0;
+        vector<int> result;
+        for(i=0;i<sizeof(nums); i++){
+            for(j=i+1;j<sizeof(nums); j++){
+                if(nums[j] == (target - nums[i]))
+                   break; 
+            }
+      
+    }
+        result.push_back(i);
+        result.push_back(j);
+        
+        return result;
+    }
+};
+// @lc code=end
+

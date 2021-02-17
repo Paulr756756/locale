a13f505c745546bd164f54bd9f95d1fb1df1d715
paulr <paulr756756@gmail.com>
2021-02-17 07:34:54 +0530

    setting up visual studio

---
+++ hackerearth/Project1/balloon.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(void) {
+	int test_cases = 0, grn = 0, prpl=0;
+	std::cin >> test_cases;
+	for (int counter = 0; counter < test_cases; counter++) {
+		std::cin >> grn >> prpl;
+		
+	}
+}

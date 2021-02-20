9315f082ba7b357733b9d4cc6bfaeb219af97226
paulr <paulr756756@gmail.com>
2021-02-20 09:47:36 +0530

    edit launch.json, tasks.json, cpp_properties.json

--- hackerearth/baloon.cpp
+++ hackerearth/baloon.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
-#include <vector>
+
 int main(void){
-    std:: cout << "Enter  your name:";  
+    int test_cases=0, grn=0, prpl=0, counter = 0, num_participants, cost=0;
+    bool temp; 
+    std::cin >> test_cases ;
+    for(int i=0; i<test_cases; i++){
+        std::cin >> grn >> prpl >> num_participants;
+        for(std::cin >> temp; counter<num_participants; counter++ ){
+            for(int j=0; j<2; j++){
+                if(j=0)
+                    if(temp)
+                        cost+=grn;
+                if(j=1)
+                    if(temp)
+                        cost+=prpl;
+            }
+        }
+    }
 }

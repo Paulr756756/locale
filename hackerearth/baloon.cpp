db3ab1da89dcd04053da181b06511ebafceeb157
paulr <paulr756756@gmail.com>
2021-02-20 10:37:07 +0530

    trial 1

--- hackerearth/baloon.cpp
+++ hackerearth/baloon.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
 int main(void){
-    int test_cases=0, grn=0, prpl=0, counter = 0, num_participants, cost=0;
-    bool temp; 
+    int test_cases=0, grn=0, prpl=0, num_participants=0, cost=0;
+    bool temp1, temp2; 
     std::cin >> test_cases ;
     for(int i=0; i<test_cases; i++){
         std::cin >> grn >> prpl >> num_participants;
-        for(std::cin >> temp; counter<num_participants; counter++ ){
-            for(int j=0; j<2; j++){
-                if(j=0)
-                    if(temp)
-                        cost+=grn;
-                if(j=1)
-                    if(temp)
-                        cost+=prpl;
-            }
+        for(int counter =0; counter<num_participants; counter++ ){
+            std::cin >> temp1 >> temp2;
+
+            if(temp1==true)
+                cost+=grn;
+            if(temp2==true)
+                cost+=prpl;
+                
         }
+    std::cout << cost << std::endl;
+    cost = 0;
+
     }
 }

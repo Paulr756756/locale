cdf42fc7102233aedc53d9fe43c729786f0760a7
paulr <paulr756756@gmail.com>
2021-03-11 11:44:47 +0530

    linked list]

---
+++ hackerearth/Algo/searching/bas.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int binary_src()
+
+int main(void){
+    int soldiers=0, b_power=0, rounds=0;
+    std::cin >> soldiers;
+    int s_power[soldiers];
+
+    for(int i=0;i<soldiers;i++)
+        std::cin >> s_power[i];
+
+    std::cin >> rounds;
+
+    for(int i=0;i<rounds;++){
+        std::cin >> b_power;
+
+    }
+    
+}
---
+++ hackerearth/Data Structures/l_l.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+int main(){
+struct Node{
+    int data;
+    Node* link;
+};
+
+//head node
+Node* A;
+A = NULL;
+
+Node* temp = new Node();
+temp->data = 2;
+temp->link = NULL;
+
+A = temp;
+
+Node* temp = new Node();
+temp->data=4;
+temp->link=NULL;
+
+}
